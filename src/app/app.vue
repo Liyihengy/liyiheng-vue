0eb63f5009ef10e82f84577c7c5eb30065af30c9
Liyiheng <liyiheng26@gmail.com>
2023-05-12 10:57:40 +0800

    复选框

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <h3>{{ name }}</h3>
-  <textarea v-model="content" placeholder="写点东西吧"></textarea>
-  <span> -> Content: {{ content }}</span>
+  <input type="checkbox" id="checked" v-model="checked" />
+  <label for="checked"> -> Checked：{{ checked }}</label>
 </template>
 
 <script>
@@ -9,7 +9,7 @@ export default {
   data() {
     return {
       name: '李依恒',
-      content: '',
+      checked: true,
     };
   },
 };
