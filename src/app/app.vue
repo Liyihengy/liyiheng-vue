329728abb3c4b66d593decf7fd7ed010f81e0700
Liyiheng <liyiheng26@gmail.com>
2023-05-12 11:32:09 +0800

    input事件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,11 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
-  <select v-model="category">
-    <option value="">》》 选择开发类型 《《</option>
-    <option value="development">》》 游戏开发 《《</option>
-    <option value="game">》》 网页开发 《《</option>
-  </select>
-  <span>-> Selece：{{ category }}</span>
+  <input type="text" @input="oneServer" />
 </template>
 
 <script>
@@ -15,6 +10,11 @@ export default {
       name: '李依恒',
       category: '',
     };
+  },
+  methods: {
+    oneServer(event) {
+      console.log(event);
+    },
   },
 };
 </script>
