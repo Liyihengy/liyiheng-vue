591469df0ee922a8de19b8096af75dcf8c2d2f6c
Liyiheng <liyiheng26@gmail.com>
2023-05-12 11:10:14 +0800

    多值复选框

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,7 +1,18 @@
 <template>
   <h3>{{ name }}</h3>
-  <input type="checkbox" id="checked" v-model="checked" />
-  <label for="checked"> -> Checked：{{ checked }}</label>
+  <label for="tag-1">
+    <input type="checkbox" id="tag-1" v-model="tags" value="大明湖" />
+    大明湖
+  </label>
+  <label for="tag-2">
+    <input type="checkbox" id="tag-2" v-model="tags" value="黑虎泉" />
+    黑虎泉
+  </label>
+  <label for="tag-3">
+    <input type="checkbox" id="tag-3" v-model="tags" value="趵突泉" />
+    趵突泉
+  </label>
+  <span>-> Tags：{{ tags }}</span>
 </template>
 
 <script>
@@ -9,7 +20,7 @@ export default {
   data() {
     return {
       name: '李依恒',
-      checked: true,
+      tags: [],
     };
   },
 };
