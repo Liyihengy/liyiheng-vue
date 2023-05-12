935886e5a5cf0407d9662db9038a4be356d30eed
Liyiheng <liyiheng26@gmail.com>
2023-05-12 11:19:54 +0800

    单选按钮

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,18 +1,24 @@
 <template>
   <h3>{{ name }}</h3>
-  <label for="tag-1">
-    <input type="checkbox" id="tag-1" v-model="tags" value="大明湖" />
-    大明湖
+  <label for="subscription-annual">
+    <input
+      type="radio"
+      id="subscription-annual"
+      v-model="subscription"
+      value="annual"
+    />
+    一年
   </label>
-  <label for="tag-2">
-    <input type="checkbox" id="tag-2" v-model="tags" value="黑虎泉" />
-    黑虎泉
+  <label for="subscription-mouth">
+    <input
+      type="radio"
+      id="subscription-mouth"
+      v-model="subscription"
+      value="mouth"
+    />
+    一月
   </label>
-  <label for="tag-3">
-    <input type="checkbox" id="tag-3" v-model="tags" value="趵突泉" />
-    趵突泉
-  </label>
-  <span>-> Tags：{{ tags }}</span>
+  <span> -> 订阅：{{ subscription }}</span>
 </template>
 
 <script>
@@ -20,7 +26,7 @@ export default {
   data() {
     return {
       name: '李依恒',
-      tags: [],
+      subscription: 'annual',
     };
   },
 };
