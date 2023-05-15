7cd711f2bb89931b81da3f5be5ccfc6ec072ed01
Liyiheng <liyiheng26@gmail.com>
2023-05-15 17:12:27 +0800

    数据管理

---
+++ src/app/app-store.ts
@@ -0,0 +1,55 @@
+import { createStore, createLogger } from 'vuex';
+import user, { UserState } from '../user/user.store';
+import { logger } from './app.plugin';
+
+export interface RootState {
+  name: string;
+  loading: boolean;
+  user?: UserState;
+}
+
+/**
+ * 创建store
+ */
+const store = createStore({
+  state: {
+    name: '',
+
+    loading: false,
+  },
+
+  getters: {
+    name(state) {
+      return `🎈 ${state.name}`;
+    },
+  },
+
+  mutations: {
+    setName(state, data) {
+      state.name = data;
+    },
+    setLoading(state, data) {
+      state.loading = data;
+    },
+  },
+
+  actions: {
+    getName({ commit, rootState }) {
+      console.log(rootState);
+      commit('setLoading', true);
+      setTimeout(() => {
+        const name = '宁皓网';
+        commit('setName', name);
+        commit('setLoading', false);
+      }, 2000);
+    },
+  },
+
+  modules: {
+    user,
+  },
+
+  plugins: [createLogger(), logger],
+});
+
+export default store;
--- src/app/app.vue
+++ src/app/app.vue
@@ -1,13 +1,45 @@
 <template>
-  <h3>{{ name }}</h3>
+  <h3 @click="onName">{{ name }} <span v-if="loading">加载中...</span></h3>
+  {{ currentUser }}
 </template>
 
 <script>
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
 export default {
   data() {
-    return {
-      name: '李依恒',
-    };
+    return {};
+  },
+
+  computed: {
+    ...mapGetters({
+      name: 'name',
+      currentUser: 'user/currentUser',
+    }),
+    ...mapState(['loading', 'user']),
+  },
+
+  created() {
+    // this.$store.dispatch('getName');
+    this.oneAa();
+    this.getCurrentUser();
+  },
+
+  methods: {
+    ...mapMutations(['setName']),
+    ...mapActions({
+      oneAa: 'getName',
+      getCurrentUser: 'user/getCurrentUser',
+    }),
+
+    onName() {
+      if (this.$store.state.name === '宁皓网') {
+        // this.$store.commit('setName', 'NINGHAO');
+        this.setName('NINGHAOWANG');
+      } else {
+        // this.$store.commit('setName', '宁皓网');
+        this.setName('宁皓网');
+      }
+    },
   },
 };
 </script>
