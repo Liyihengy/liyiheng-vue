5e3b4d202bc520262066f387809db79aae096034
Liyiheng <liyiheng26@gmail.com>
2023-05-16 17:41:04 +0800

    监视数据

--- src/app/app.vue
+++ src/app/app.vue
@@ -4,17 +4,25 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 export default {
   setup() {
+    //数据
     const name = ref('宁皓网');
 
+    //方法
     const changeName = () => {
       name.value = 'NINGHAOWANG';
     };
 
+    //生命周期
     onMounted(() => {
       console.log('mouted');
+    });
+
+    //监视数据
+    watch(name, (newName, oldName) => {
+      console.log(newName, oldName);
     });
 
     return {
