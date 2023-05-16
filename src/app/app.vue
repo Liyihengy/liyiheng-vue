87a4c03196b511436db100aa5251ac0ca717598d
Liyiheng <liyiheng26@gmail.com>
2023-05-16 17:26:05 +0800

    使对象具有反应特性的数据（reactive）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,21 @@
 <template>
   <h3 @click="name = 'NINGHAOWANG'">{{ name }}</h3>
+  <div @click="user.name = '李依恒'">{{ user.name }}</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 export default {
   setup() {
     const name = ref('宁皓网');
 
+    const user = reactive({
+      name: 'LIYIHENG',
+    });
+
     return {
       name,
+      user,
     };
   },
 };
