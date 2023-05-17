8e61787e53d8810ac660dd4e397346dd4ffbaea0
Liyiheng <liyiheng26@gmail.com>
2023-05-17 12:28:25 +0800

    在请求里配置头部数据

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
+  <input type="text" v-model="title" @keyup.enter="createPost" />
   <div>{{ errorMessage }}</div>
   <div v-for="post in posts" :key="post.id">
     {{ post.title }} - {{ post.user.name }} - {{ post.content }}
@@ -20,19 +21,12 @@ export default {
         password: '1313',
       },
       token: '',
+      title: '',
     };
   },
 
   async created() {
-    try {
-      const response = await apiHttpClient.get('/posts');
-
-      console.log(apiHttpClient.defaults);
-
-      this.posts = response.data;
-    } catch (error) {
-      this.errorMessage = error.message;
-    }
+    this.getPost();
 
     //用户登录
     try {
@@ -43,6 +37,39 @@ export default {
     } catch (error) {
       this.errorMessage = error.message;
     }
+  },
+
+  methods: {
+    async getPost() {
+      try {
+        const response = await apiHttpClient.get('/posts');
+
+        console.log(apiHttpClient.defaults);
+
+        this.posts = response.data;
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
+
+    async createPost() {
+      try {
+        const response = await apiHttpClient.post(
+          '/posts',
+          {
+            title: this.title,
+          },
+          { headers: { Authorization: `Bearer ${this.token}` } },
+        );
+
+        console.log(response.data);
+
+        this.title = '';
+        this.getPost();
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    },
   },
 };
 </script>
