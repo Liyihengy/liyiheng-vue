6e1e4bff1ebc8ed33be21c2687b187f5ecb48714
Liyiheng <liyiheng26@gmail.com>
2023-05-17 11:03:11 +0800

    安装与使用HTTP客户端请求服务端接口&处理使用axios发送请求时的错误

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,13 +1,37 @@
 <template>
   <h3>{{ name }}</h3>
+  <div>{{ errorMessage }}</div>
+  <div v-for="post in posts" :key="post.id">
+    {{ post.title }} - {{ post.user.name }} - {{ post.content }}
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       name: '李依恒',
+      posts: [],
+      errorMessage: '',
     };
+  },
+
+  created() {
+    axios
+      .get('http://localhost:3000/posts1')
+      .then((response) => {
+        console.log(response);
+
+        this.posts = response.data;
+      })
+      .catch((error) => {
+        console.log(error.message);
+        console.log(error.response);
+
+        this.errorMessage = error.message;
+      });
   },
 };
 </script>
