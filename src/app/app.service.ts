1a4b2d5f3d99270cd59f21b64419235a34c1b9de
Liyiheng <liyiheng26@gmail.com>
2023-05-23 14:14:33 +0800

    安装配置Socket.io客户端

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { API_BASE_URL } from './app.config';
+import { io } from 'socket.io-client';
 
 /**
  * HTTP客户端
@@ -7,3 +8,8 @@ import { API_BASE_URL } from './app.config';
 export const apiHttpClient = axios.create({
   baseURL: API_BASE_URL as string,
 } as AxiosRequestConfig);
+
+/**
+ *Socket
+ */
+export const socket = io(API_BASE_URL);
